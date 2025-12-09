b1cdcc11d2ae6e9bc9c744405c09f3d4143e78d8
001parthdev-code <001.parth.dev@gmail.com>
2025-12-09 10:14:15 +0530

    Next word predictor

--- Next_Word_Predictor/Run.py
+++ Next_Word_Predictor/Run.py
@@ -1,12 +1,16 @@
 from main import build_corpus, build_bigram, build_trigram, predict_next
 
-corpus = build_corpus()
-bigram = build_bigram(corpus)
-trigram = build_trigram(corpus)
 
-print("he →", predict_next("he", trigram, bigram))
-print("ha →", predict_next("ha", trigram, bigram))
-print("al →", predict_next("al", trigram, bigram))
-print("h  →", predict_next("h", trigram, bigram))
-print("e  →", predict_next("e", trigram, bigram))
-print("ll →", predict_next("ll", trigram, bigram))
+if __name__ == "__main__":
+    corpus = build_corpus()
+    bigram = build_bigram(corpus)
+    trigram = build_trigram(corpus)
+
+    while True:
+        text = input("Enter text (or quit):" ).strip()
+        if text.lower() == "quit":
+            break
+
+        result = predict_next(text,trigram,bigram)
+        print("Next character:", result)
+
